3415a8d39f88d585e5c395b6e077d59cc8e59765
Henna Maria <hennamaria2001@gmail.com>
2024-08-07 12:49:33 +0530

    font added

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,13 +1,16 @@
 'use client';
 import { useRouter } from "next/navigation";
+import { Monomaniac_One } from "next/font/google";
 
+const monomaniac = Monomaniac_One({subsets: ["latin-ext"], weight: "400"});
 const About = () => {
   const route = useRouter();
   return (
     <section className='grid place-items-center gap-5'>
       <div>About</div>
       <button 
-      onClick={route.push('contact')}
+      className={`text-${monomaniac.className}`}  // Using the font class obtained from Monomaniac_One
+      // onClick={route.push('contact')}
       >Go back to the contact</button>
     </section>
   )
