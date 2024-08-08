ed7cc3b0da8e47e767ce766bfc5fcb8a313db33a
Henna Maria <hennamaria2001@gmail.com>
2024-08-08 14:00:49 +0530

    user api added

--- app/api/users/[id]/route.js
+++ app/api/users/[id]/route.js
@@ -1,9 +1,24 @@
+import { users } from "@/app/utils/data";
 import { NextResponse } from "next/server";
 
-export const GET = (_, res) => {
-    console.log(res.params.id); //2
-    console.log(res) //{ params: { id: '2' } }
-    return NextResponse.json({
-        message: 'Hello, Next.js Serverless Function!'
-    })
+export const GET = async (_, res) => {
+    try {
+        const user = users.filter(user => user.id === Number(res.params.id));
+        if(user) {
+            return NextResponse.json({
+                success: true,
+                data: user
+            }, {status: 200})
+        } else {
+            return NextResponse.json({
+                success: false,
+                message: "User not found"
+            }, {status: 404})
+        }
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            message: error.message
+        }, { status: 500 });
+    }
 }
--- app/api/users/route.js
+++ app/api/users/route.js
@@ -2,5 +2,8 @@ import { NextResponse } from "next/server"
 import { users } from "@/app/utils/data"
 
 export const GET = () => {
-    return NextResponse.json(users, { status: 200 }) //we don't want need to provide status code
+    return NextResponse.json({
+        success: true,
+        data: users
+    }, { status: 200 }) 
 }
