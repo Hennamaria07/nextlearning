b42b2dba4674fda6c0c6074ad50931be8bc6a393
Henna Maria <hennamaria2001@gmail.com>
2024-08-17 13:49:38 +0530

    fs added

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,9 +1,49 @@
 import { NextResponse } from "next/server"
 import { users } from "@/app/utils/data"
+import fs from 'fs'
 
 export const GET = () => {
     return NextResponse.json({
         success: true,
         data: users
     }, { status: 200 }) 
+}
+
+export const POST = async (req, res) => {
+    try {
+        const {name, email, password, id } = await req.json();
+        if([name, email, password, id].some(field => !field || field.length === 0)) {
+            return NextResponse.json({
+                success: false,
+                message: "All fields are required and cannot be empty"
+            }, { status: 400 })
+        } else if (typeof id !== "number") {
+            return NextResponse.json({
+                success: false,
+                message: "ID must be a number"
+            }, { status: 400 })
+        } else if (id) {
+            const userExists = users.find(user => user.id === id);
+            if(userExists) {
+                return NextResponse.json({
+                    success: false,
+                    message: "User with this ID already exists",
+                }, { status: 409 })
+            }
+        }
+        users.push({name, email, password, id});
+
+        fs.writeFileSync('./app/utils/data.js', `export const users = ${JSON.stringify(users, null, 2)}`, 'utf-8');
+        
+        return NextResponse.json({
+            success: true,
+            message: "User created successfully",
+            data: users
+        }, { status: 201 })  // 201 Created
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            message: error.message
+        }, { status: 500})
+    }
 }
--- app/utils/data.js
+++ app/utils/data.js
@@ -1,72 +1,78 @@
 export const users = [
-    {
-        id: 1,
-        name: "Luffy",
-        age: 20,
-        email: "luffy@gmail.com",
-        password: "luffy",
-    },
-    {
-        id: 2,
-        name: "Zoro",
-        age: 21,
-        email: "zoro@gmail.com",
-        password: "zoro",
-    },
-    {
-        id: 3,
-        name: "Nami",
-        age: 20,
-        email: "nami@gmail.com",
-        password: "nami",
-    },
-    {
-        id: 4,
-        name: "Sanji",
-        age: 21,
-        email: "sanji@gmail.com",
-        password: "sanji",
-    },
-    {
-        id: 5,
-        name: "Usopp",
-        age: 19,
-        email: "usopp@gmail.com",
-        password: "usopp",
-    },
-    {
-        id: 6,
-        name: "Chopper",
-        age: 17,
-        email: "chopper@gmail.com",
-        password: "chopper",
-    },
-    {
-        id: 7,
-        name: "Robin",
-        age: 28,
-        email: "robin@gmail.com",
-        password: "robin",
-    },
-    {
-        id: 8,
-        name: "Franky",
-        age: 34,
-        email: "franky@gmail.com",
-        password: "franky",
-    },
-    {
-        id: 9,
-        name: "Brook",
-        age: 90,
-        email: "brook@gmail.com",
-        password: "brook",
-    },
-    {
-        id: 10,
-        name: "Jinbei",
-        age: 46,
-        email: "jinbei@gmail.com",
-        password: "jinbei",
-    }
-];
+  {
+    "id": 1,
+    "name": "Luffy",
+    "age": 20,
+    "email": "luffy@gmail.com",
+    "password": "luffy"
+  },
+  {
+    "id": 2,
+    "name": "Zoro",
+    "age": 21,
+    "email": "zoro@gmail.com",
+    "password": "zoro"
+  },
+  {
+    "id": 3,
+    "name": "Nami",
+    "age": 20,
+    "email": "nami@gmail.com",
+    "password": "nami"
+  },
+  {
+    "id": 4,
+    "name": "Sanji",
+    "age": 21,
+    "email": "sanji@gmail.com",
+    "password": "sanji"
+  },
+  {
+    "id": 5,
+    "name": "Usopp",
+    "age": 19,
+    "email": "usopp@gmail.com",
+    "password": "usopp"
+  },
+  {
+    "id": 6,
+    "name": "Chopper",
+    "age": 17,
+    "email": "chopper@gmail.com",
+    "password": "chopper"
+  },
+  {
+    "id": 7,
+    "name": "Robin",
+    "age": 28,
+    "email": "robin@gmail.com",
+    "password": "robin"
+  },
+  {
+    "id": 8,
+    "name": "Franky",
+    "age": 34,
+    "email": "franky@gmail.com",
+    "password": "franky"
+  },
+  {
+    "id": 9,
+    "name": "Brook",
+    "age": 90,
+    "email": "brook@gmail.com",
+    "password": "brook"
+  },
+  {
+    "id": 10,
+    "name": "Jinbei",
+    "age": 46,
+    "email": "jinbei@gmail.com",
+    "password": "jinbei"
+  },
+  {
+    "name": "Sabo",
+    "email": "sabo@gmail.com",
+    "password": "sabo",
+    "id": 13
+  }
+]
